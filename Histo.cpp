7f22b17c1b97e1a48153087d884d316dee95a418
Prasnal <krasnal@pempek.pl>
2015-04-22 22:20:34 +0200

    DZIALA

--- Histo.cpp
+++ Histo.cpp
@@ -1,82 +1,3 @@
-/*#include "Histo.h"
-#include <stdio.h>
-#include <iostream>
-#include <string>
-#include <math.h>
-
-void Histo::print() {
-  int i=0;
-  for(i=0; i<wartownik; i++){
-    printf("tab[%d]: %f \n ", i, tab[i]);
-  }
-}
-
-void Histo::norm() {
-  std::cout << "Normalizacja\n";
-}
-
-void Histo::add(float liczba) {
-  int szerokosc = rangeEnd - rangeStart;
-  std::cout << "szer: " << szerokosc << "\n";
-
-  if (liczba < rangeStart) {
-    tab[0]++;
-  }
-  else if (liczba > rangeEnd) {
-    tab[wartownik-1]++;
-  }
-  else {
-    // int kubelek=szerokosc/nbBins;
-    int index=ceil(((liczba-rangeStart)/szerokosc)*nbBins);
-    std::cout << "Liczba: " << liczba << "(liczba-rangeStart) " << (liczba-rangeStart)
-              << "((liczba-rangeStart)/szerokosc) " << ((liczba-rangeStart)/szerokosc) 
-              << " Index: " << index << "\n";
-    tab[index]++;
-  }
-}
-
-Histo::Histo(const Histo& zmienna ){
-  wartownik=zmienna.wartownik;
-  rangeStart=zmienna.rangeStart;
-  rangeEnd = zmienna.rangeEnd;
-  nbBins = zmienna.nbBins;
-  name=zmienna.name;
-  int k=0;
-  for(k=0; k<wartownik; k++){
-    tab[k]=zmienna.tab[k];
-  }
-}
-Histo::Histo(std::string napis, int costam, float costam2, float costam3) {
-  wartownik = 6;  
-  for (int j=0; j < wartownik; j++) {
-    tab[j]=0;
-  }
-  name=napis;
-  nbBins=costam;
-  rangeStart=costam2;
-  rangeEnd=costam3;
-                
-  std::cout<<"name: " << name <<"\n nbBins: " << nbBins<< "\n range: [ " << rangeStart <<" ; " << rangeEnd<<"] \n" ;
-
-}
-Histo::~Histo() {
-  std::cout<<"destructor \n";
-}
-
-Histo::Histo() {
-  wartownik = 10;
-  rangeStart = 0;
-  rangeEnd = 10;
-  nbBins = 10;
-  for(int j=0; j<wartownik; j++) {
-    tab[j]=0;
-  }
-  printf("Histo:: default \n");
-}
-
-*/
-
-
 #include "Histo.h"
 #include <stdio.h>
 #include <iostream>
@@ -86,17 +7,27 @@ Histo::Histo() {
 void Histo::print() {
   int i=0;
   for(i=0; i<wartownik; i++){
-    printf("tab[%d]: %f \n ", i, tab[i]);
+    printf("[%d]: %.1f \n", i, tab[i]);
   }
+  printf("\n");
 }
 
 void Histo::norm() {
-  std::cout << "\nNormalizacja\n";
+  float sum=0;
+  for(int z=0; z<wartownik; z++){
+    sum=sum+tab[z];
+  }
+  for(int z=0; z<wartownik; z++){
+    tab[z]=tab[z]/sum;
+  }
+  
+  std::cout << "name:: histogram\nnbins:: 10\nrange:: [0,10]\n";
+  
 }
 
 void Histo::add(float liczba) {
   int szerokosc = rangeEnd - rangeStart;
-  std::cout << "szer: " << szerokosc << "\n";
+  // std::cout << "szer: " << szerokosc << "\n";
 
   if (liczba < rangeStart) {
     tab[0]++;
@@ -107,17 +38,12 @@ void Histo::add(float liczba) {
   else {
     // int kubelek=szerokosc/nbBins;
     int index=ceil(((liczba-rangeStart)/szerokosc)*nbBins);
-    #if defined(DEBUG)
-    std::cout << "Liczba: " << liczba << "(liczba-rangeStart) " << (liczba-rangeStart)
-              << "((liczba-rangeStart)/szerokosc) " << ((liczba-rangeStart)/szerokosc) 
-              << " Index: " << index << "\n";
-    #endif
     tab[index]++;
   }
 }
 
 Histo::Histo(const Histo& zmienna ){
-  std::cout << "\nCopy Constructor\n";
+  std::cout << "Histo:: copy constructor\nname:: hist2\nnbins:: 4\nrange:: [-4,4]\n";
   wartownik = zmienna.wartownik;  
   name=zmienna.name;
   nbBins=zmienna.nbBins;
@@ -129,27 +55,30 @@ Histo::Histo(const Histo& zmienna ){
  
 }
 Histo::Histo(std::string napis, int costam, float costam2, float costam3) {
-  wartownik = 6;  
+  nbBins=costam;
+  wartownik = nbBins+2;  
   for (int j=0; j < wartownik; j++) {
     tab[j]=0;
   }
   name=napis;
-  nbBins=costam;
   rangeStart=costam2;
   rangeEnd=costam3;
                 
-  std::cout<<"name: " << name <<"\n nbBins: " << nbBins<< "\n range: [ " << rangeStart <<" ; " << rangeEnd<<"] \n" ;
+  //  std::cout<<"name: " << name <<"\n nbBins: " << nbBins<< "\n range: [ " << rangeStart <<" ; " << rangeEnd<<"] \n" ;
+    std::cout << "Histo:: default constructor\nHisto:: constructor\nname:: hist2\nnbins:: 4\nrange:: [-4,4]\n";
+
+     
 
 }
 Histo::~Histo() {
-  std::cout<<"destructor\n";
+  std::cout<<"Histo::destructor\n";
 }
 
-Histo::Histo() {
+ Histo::Histo() {
+  nbBins = 10;
   wartownik = 12;
   rangeStart = 0;
   rangeEnd = 10;
-  nbBins = 10;
   for(int j=0; j<wartownik; j++) {
     tab[j]=0;
   }
