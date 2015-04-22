8eccf9234f89238f5df7884fe79df72de8cec424
Prasnal <krasnal@pempek.pl>
2015-04-22 19:30:22 +0200

    Commit

--- Histo.h
+++ Histo.h
@@ -1,18 +1,24 @@
 #ifndef Histo_h
 #define Histo_h
 #include <string>
+#include <math.h>
+#define SIZE 100
 
 class Histo{
 public:
-  float tab[];
+  float tab[SIZE];
+  int  wartownik;
+  std::string name;
+  int  nbBins;
+  float  rangeStart;
+  float  rangeEnd;
   void print();
   void norm();
   void add(float liczba);
   Histo(const Histo& zmienna );
-  Histo(const Histo& zmienna, std::string napis, int costam, float costam2, float costam3);
-  ~Histo();
+  Histo(std::string napis, int costam, float costam2, float costam3);
   Histo();
-
+  ~Histo();
 
 };
 
